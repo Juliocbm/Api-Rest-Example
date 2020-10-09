31b8fb0bd8757f939e7686ab34b394e309a309e6
Jose Carlos Gonzalez Nieto <cgonzalez@grupohg.local>
2020-10-09 14:52:40 -0500

    Inicializacion

---
+++ ApiPrueba/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using ApiPrueba.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ApiPrueba
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración de rutas y servicios de API
+            config.MapHttpAttributeRoutes();
+
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
+
---
+++ ApiPrueba/Controllers/ViajesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiPrueba.Controllers
+{
+        /// <summary>
+        /// customer controller class for testing security token
+        /// </summary>
+        [Authorize]
+        [RoutePrefix("api/viajes")]
+        public class ViajesController : ApiController
+        {
+            [HttpGet]
+            public IHttpActionResult GetId(int id)
+            {
+                var viajeFake = "viaje-fake";
+                return Ok(viajeFake);
+            }
+
+            [HttpGet]
+            public HttpResponseMessage GetAll()
+            {
+                var viajesFake = new string[] { "viaje-1", "viaje-2", "viaje-3" };
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, viajesFake);
+
+
+                return response;
+            }
+        }
+    
+
+}
